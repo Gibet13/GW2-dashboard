458e7ba7f771e1766476e490ed027056e190b43b
Gibet13 <48248176+Gibet13@users.noreply.github.com>
2022-01-12 16:18:31 +0100

    daily achievement display

--- gw2_api_app/gw2_alpha/static/gw2_alpha/index.js
+++ gw2_api_app/gw2_alpha/static/gw2_alpha/index.js
@@ -1,5 +1,25 @@
-fetch('https://api.guildwars2.com/v2/items?ids=89271')
+fetch('https://api.guildwars2.com/v2/achievements/daily')
 .then(response => response.json())
-.then(result => {
-    console.log(result);
+.then(dailies => {
+    console.log(dailies);
+    dailiesList = document.getElementById('dailies');
+        
+    for(let key of Object.keys(dailies)) {
+        category = document.createElement('div');
+        category.setAttribute('id',`${key}`);
+        category.innerHTML = `<h3>${key}</h3>`;
+        
+        dailies[key].forEach(element => {
+
+            fetch(`https://api.guildwars2.com/v2/achievements?id=${element.id}`)
+            .then(response => response.json())
+            .then(result => {
+
+                daily = document.createElement('p');
+                daily.innerHTML = `${result.name}`;
+                document.getElementById(key).append(daily)
+            })
+        });
+        dailiesList.append(category);
+    }
 });
