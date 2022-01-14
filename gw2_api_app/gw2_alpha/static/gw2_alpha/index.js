d51c6323d8a2ccc819819524cc10e5eaea5f0ce7
Gibet13 <48248176+Gibet13@users.noreply.github.com>
2022-01-15 00:37:55 +0100

    daily achievements
    Today and tomorrow

--- gw2_api_app/gw2_alpha/static/gw2_alpha/index.js
+++ gw2_api_app/gw2_alpha/static/gw2_alpha/index.js
@@ -1,25 +1,62 @@
-fetch('https://api.guildwars2.com/v2/achievements/daily')
-.then(response => response.json())
-.then(dailies => {
-    console.log(dailies);
-    dailiesList = document.getElementById('dailies');
-        
-    for(let key of Object.keys(dailies)) {
-        category = document.createElement('div');
-        category.setAttribute('id',`${key}`);
-        category.innerHTML = `<h3>${key}</h3>`;
-        
-        dailies[key].forEach(element => {
+document.addEventListener('DOMContentLoaded', (event) => {
 
-            fetch(`https://api.guildwars2.com/v2/achievements?id=${element.id}`)
-            .then(response => response.json())
-            .then(result => {
-
-                daily = document.createElement('p');
-                daily.innerHTML = `${result.name}`;
-                document.getElementById(key).append(daily)
-            })
-        });
-        dailiesList.append(category);
-    }
+    document.getElementById("daily_today").onclick =function() {load_dailies("")};
+    document.getElementById("daily_tomorrow").onclick = function() {load_dailies('/tomorrow')};
 });
+
+
+function load_dailies(date) {
+    
+    fetch(`https://api.guildwars2.com/v2/achievements/daily${date}`)
+    .then(response => response.json())
+    .then(dailies => {
+        
+        dailiesList = document.getElementById('dailies');
+        dailiesList.innerHTML = "";
+
+        for(let key of Object.keys(dailies)) {
+            category = document.createElement('div');
+            category.setAttribute('id',`${key}`);
+            category.innerHTML = `<h3>${key}</h3>`;
+            
+            dailies[key].forEach(element => {
+
+                fetch(`https://api.guildwars2.com/v2/achievements?id=${element.id}`)
+                .then(response => response.json())
+                .then(result => {
+
+                    daily = document.createElement('div');
+                    daily.classList.add('achievement');
+                    daily.innerHTML = `${result.name}`;
+                    document.getElementById(key).append(daily)
+                    daily.onclick = function() {load_achievement(element.id)};
+                    
+                })
+                
+                dailiesList.append(category);
+            }); 
+        }
+    });
+}
+
+function load_categories() {
+    fetch("https://api.guildwars2.com/v2/achievements/categories")
+}
+
+function load_achievement(id) {
+    fetch(`https://api.guildwars2.com/v2/achievements?id=${id}`)
+    .then(response => response.json())
+    .then(result => {
+        const details = document.getElementById('details');
+        details.innerHTML = "";
+        const name = document.createElement('div');
+
+        name.innerHTML = result.name
+
+        const descryption = document.createElement('div');
+        descryption.innerHTML = result.requirement;
+
+        details.append(name, descryption)
+    
+    })
+}
