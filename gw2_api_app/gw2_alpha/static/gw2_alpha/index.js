6e40b0c786fd10e7ec1e0afe258ef7168378152b
Gibet13 <48248176+Gibet13@users.noreply.github.com>
2022-01-17 01:58:34 +0100

    accordion display

--- gw2_api_app/gw2_alpha/static/gw2_alpha/index.js
+++ gw2_api_app/gw2_alpha/static/gw2_alpha/index.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', (event) => {
 
-    document.getElementById("daily_today").onclick =function() {load_dailies("")};
-    document.getElementById("daily_tomorrow").onclick = function() {load_dailies('/tomorrow')};
+    //document.getElementById("daily_today").onclick =function() {load_dailies("")};
+    //document.getElementById("daily_tomorrow").onclick = function() {load_dailies('/tomorrow')};
 
     load_groups()
 });
@@ -55,7 +55,7 @@ function load_groups() {
                 
                 group_tab = document.createElement('div');
                 group_tab.innerHTML = group.name;
-                group_tab.setAttribute('id', group.name);
+                group_tab.setAttribute('id', `grp${group.id}`);
                 group_tab.onclick = function() {load_categories(group.id)};
                 group_tab.classList.add('achievement');
 
@@ -68,6 +68,7 @@ function load_groups() {
 function load_categories(group_id) {
 
     cat_nav = document.getElementById('cat_navbar');
+    document.getElementById('achievements').innerHTML = "";
     cat_nav.innerHTML = "";
 
     fetch(`https://api.guildwars2.com/v2/achievements/groups/${group_id}`)
@@ -80,7 +81,7 @@ function load_categories(group_id) {
                 
                 cat_tab = document.createElement('div');
                 cat_tab.innerHTML = category.name;
-                cat_tab.setAttribute('id', category.name);
+                cat_tab.setAttribute('id', `cat${category.id}`);
                 cat_tab.onclick = function() {load_category(category.id)};
                 cat_tab.classList.add('achievement');
 
@@ -104,10 +105,39 @@ function load_category(cat_id) {
             .then(achievement => {
                 
                 ach_box = document.createElement('div');
-                ach_box.innerHTML = achievement.name;
-                ach_box.setAttribute('id', achievement.name);
-                ach_box.classList.add('achievement');
+                ach_box.classList.add('accordion-item');
+                ach_box.setAttribute('id', `ach${achievement.id}`);
 
+                ach_header = document.createElement('h2');
+                ach_header.setAttribute('id', `head${achievement.id}`);
+                ach_header.classList.add('accordion-header');
+                ach_box.append(ach_header);
+
+                ach_button = document.createElement('button');
+                ach_button.classList.add('accordion-button','collapsed');
+                ach_button.setAttribute('type', 'button');
+                ach_button.setAttribute('data-bs-toggle', 'collapse');
+                ach_button.setAttribute('data-bs-target', `#coll${achievement.id}`);
+                ach_button.setAttribute('aria-expanded', 'true');
+                ach_button.setAttribute('aria-controls', `coll${achievement.id}`);
+                ach_button.innerHTML = achievement.name;
+                ach_header.append(ach_button);
+
+                ach_collapse = document.createElement('div');
+                ach_collapse.classList.add('accordion-collapse','collapse');
+                ach_collapse.setAttribute('id',`coll${achievement.id}`);
+                ach_collapse.setAttribute('aria-labelledby',`head${achievement.id}`);
+                ach_collapse.setAttribute('data-bs-parent','achievements')
+                ach_box.append(ach_collapse);
+
+                ach_body = document.createElement('div');
+                ach_body.classList.add('accordion-body')
+                ach_body.innerHTML = achievement.requirement;
+                ach_collapse.append(ach_body);
+
+                
+                //ach_box.onclick = function() {load_achievement(achievement.id)};
+                
                 cat_div.append(ach_box)
             })
         })
@@ -118,16 +148,20 @@ function load_achievement(id) {
     fetch(`https://api.guildwars2.com/v2/achievements?id=${id}`)
     .then(response => response.json())
     .then(result => {
-        const details = document.getElementById('details');
-        details.innerHTML = "";
-        const name = document.createElement('div');
+        details = document.getElementById(`ach${result.id}`);
 
-        name.innerHTML = result.name
+        descryption = document.createElement('div');
+        descryption.setAttribute('id', `desc${result.id}`)
+        descryption.innerHTML = "";
+        descryption.classList.add('achievement');
+        descryption.innerHTML = `${result.requirement}`;
 
-        const descryption = document.createElement('div');
-        descryption.innerHTML = result.requirement;
-
-        details.append(name, descryption)
+        if(!document.getElementById(`ach${result.id}`)){
+            details.append(descryption);
+        }
+        else{
+            //details.parentNode.removeChild(document.getElementById(`desc${result.id}`));
+        }
     
     })
 }
