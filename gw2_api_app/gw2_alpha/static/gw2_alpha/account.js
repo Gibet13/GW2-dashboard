c892fefaa7baf6bb6f326f0308bd3e05e15ddb70
Gibet13 <48248176+Gibet13@users.noreply.github.com>
2022-01-27 23:47:23 +0100

    react

--- gw2_api_app/gw2_alpha/static/gw2_alpha/account.js
+++ gw2_api_app/gw2_alpha/static/gw2_alpha/account.js
@@ -1,3 +1,5 @@
+'use strict';
+
 document.addEventListener('DOMContentLoaded', (event) => {
     document.getElementById('api_key_test').onclick = function() {api_key_test()};
 
@@ -9,7 +11,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
 function api_key_test(){
 
-    key = document.getElementById('api-key').value;
+    let key = document.getElementById('api-key').value;
     fetch(`https://api.guildwars2.com/v2/account?access_token=${key}`)
     .then(response => response.json())
     .then(result => {
@@ -32,8 +34,33 @@ function load_characters(api_key) {
     fetch(`https://api.guildwars2.com/v2/characters?access_token=${api_key}`)
     .then(response => response.json())
     .then(result => {
+
+        console.log(result);
+        let characters = document.querySelector('#characters');
+
+        result.forEach(element => {
+            let character = document.createElement('div');
+            character.innerHTML = element;
+            character.onclick = function() {load_character(api_key, element)};
+            characters.append(character);
+
+        });
+    })
+}
+
+function load_character(api_key, name){
+
+    character = {}
+    equipement = {}
+    build = {}
+
+    fetch(`https://api.guildwars2.com/v2/characters/${name}?access_token=${api_key}`)
+    .then(response => response.json())
+    .then(result => {
         console.log(result)
     })
+
+    return(character);
 }
 
 function load_bank(api_key) {
@@ -50,3 +77,183 @@ function toggle_tab (tab_id) {
         tab.style.display = 'none';
     }
 }
+
+/*function showcharactersheet(character, build) {
+
+
+    return(
+        <div id="character_sheet" class="container">
+            <div id="account_header">
+                <div>
+                    <h1>Character Name</h1>
+                </div>
+                <div id="chara_navigation">
+                    <div  id="chara_tabs">Info</div>
+                    <div id="chara_info">
+                        <ul>
+                            <div class="achievement" id="menu_gear">Gear</div>
+                            <div class="achievement" id="menu_build">Build</div>
+                            <div class="achievement">Inventory</div>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <hr></hr>
+            <div id="gear">
+                <h2>Gear</h2>
+                <div id="gear_view">
+                    <div id="main_gear">
+                        <div id="armor_piece">
+                            Armor
+                            <div class="icon">1</div>
+                            <div class="icon">2</div>
+                            <div class="icon">3</div>
+                            <div class="icon">4</div>
+                            <div class="icon">5</div>
+                            <div class="icon">6</div>
+                        </div>
+                        <div id="land_weapons_set">
+                            Weapon Set
+                            <div id="set_1">
+                                set 1
+                                <div class="icon">1</div>
+                                <div class="icon">2</div>
+                            </div>
+                            <div id="set_2">
+                                set 2
+                                <div class="icon">1</div>
+                                <div class="icon">2</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div id="secondary_gear">
+                        Stats
+                        <div id="stat_view">
+                            Attributes
+                        </div>
+                        <div id="trinkets">
+                            Trinkets
+                            <div>
+                                <div id="backpiece" class="icon">1</div>
+                                <div id="accesory">
+                                    <div class="icon">1</div>
+                                    <div class="icon">2</div>
+                                </div>
+                            </div>
+                            <div>
+                                <div id="amulet" class="icon">1</div>
+                                <div id="rings">
+                                    <div class="icon">1</div>
+                                    <div class="icon">2</div>
+                                </div>
+                            </div>
+                            Underwater
+                            <div id="underwater_gear">
+                                <div>
+                                    <div class="icon">1</div>
+                                    <div class="icon">1</div>
+                                    <div class="icon">2</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div id="inventory" class="container">
+
+            </div>
+            
+            <div id="build">
+                <h2>Build</h2>
+                <div id="skill-bars">
+                    <div class="skill_bar">
+                        
+                        <div class="off_hand">
+                            <div class="icon">1</div>
+                            <div class="icon">1</div>
+                            <div class="icon">2</div>
+                            <div class="icon">3</div>
+                            <div class="icon">1</div>
+                        </div>
+                    </div>
+                  
+                    <div class="skill_bar">
+                        
+                        <div class="off_hand">
+                            <div class="icon">1</div>
+                            <div class="icon">1</div>
+                            <div class="icon">2</div>
+                            <div class="icon">3</div>
+                            <div class="icon">1</div>
+                        </div>
+                    </div>
+                </div>
+                <div id="trait-lines">
+                    <div class="traitline" id="topline">
+                        <div class="traitline_icon">1</div>
+                        <div class="minor_trait_icon"></div>
+                        <div class="trait_tier">
+                            <div class="major_trait_icon"></div>
+                            <div class="major_trait_icon"></div>
+                            <div class="major_trait_icon"></div>
+                        </div>
+                        <div class="minor_trait_icon"></div>
+                        <div class="trait_tier">
+                            <div class="major_trait_icon"></div>
+                            <div class="major_trait_icon"></div>
+                            <div class="major_trait_icon"></div>
+                        </div>
+                        <div class="minor_trait_icon"></div>
+                        <div class="trait_tier">
+                            <div class="major_trait_icon"></div>
+                            <div class="major_trait_icon"></div>
+                            <div class="major_trait_icon"></div>
+                        </div>
+                    </div>
+                    <div class="traitline" id="midline">
+                        <div class="traitline_icon">2</div>
+                        <div class="minor_trait_icon"></div>
+                        <div class="trait_tier">
+                            <div class="major_trait_icon"></div>
+                            <div class="major_trait_icon"></div>
+                            <div class="major_trait_icon"></div>
+                        </div>
+                        <div class="minor_trait_icon"></div>
+                        <div class="trait_tier">
+                            <div class="major_trait_icon"></div>
+                            <div class="major_trait_icon"></div>
+                            <div class="major_trait_icon"></div>
+                        </div>
+                        <div class="minor_trait_icon"></div>
+                        <div class="trait_tier">
+                            <div class="major_trait_icon"></div>
+                            <div class="major_trait_icon"></div>
+                            <div class="major_trait_icon"></div>
+                        </div>
+                    </div>
+                    <div class="traitline" id="bottomline">
+                        <div class="traitline_icon">3</div>
+                        <div class="minor_trait_icon"></div>
+                        <div class="trait_tier">
+                            <div class="major_trait_icon"></div>
+                            <div class="major_trait_icon"></div>
+                            <div class="major_trait_icon"></div>
+                        </div>
+                        <div class="minor_trait_icon"></div>
+                        <div class="trait_tier">
+                            <div class="major_trait_icon"></div>
+                            <div class="major_trait_icon"></div>
+                            <div class="major_trait_icon"></div>
+                        </div>
+                        <div class="minor_trait_icon"></div>
+                        <div class="trait_tier">
+                            <div class="major_trait_icon"></div>
+                            <div class="major_trait_icon"></div>
+                            <div class="major_trait_icon"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}*/
